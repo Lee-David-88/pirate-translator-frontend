9664800c4127d1a08b75fb47e0049453e6aed649
Lee-David-88 <leedavid19990430@gmail.com>
2022-04-03 22:50:31 -0700

    fix home page

--- src/Home.js
+++ src/Home.js
@@ -35,40 +35,41 @@ export default function Home() {
 	return (
 		<>
 		<div class="background-body">
-		<Navbar expand="lg">
-  			<Container>
-    			<Navbar.Brand href="/login">Pirate Translator</Navbar.Brand>
-    			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-    			<Navbar.Collapse id="basic-navbar-nav">
-      			<Nav className="me-auto">
-        			<Nav.Link href="/docs">Documentation</Nav.Link>
-					{client.isAdmin ? (
-						<Nav.Link href="/admin">Admin</Nav.Link>
-					) : null}
-					<Nav.Link href="">Client ID: {client.id}</Nav.Link>
-      			</Nav>
-    			</Navbar.Collapse>
-  			</Container>
-		</Navbar>
-		<img class="main-logo" src="piratetext.png"></img>
-		<div class="center">
-		<p>
-			<input
-				value={prompt}
-				onChange={(e) => setPrompt(e.target.value)}
-				type="text"
-				placeholder="Enter The Prompt You Wish To Translate Here"
-			/>
-		</p>
-		<p>
-			<button disabled={isPending} onClick={translate}>
-				Translate
-			</button>
-			{error ? <p style={{ color: 'red' }}>{error}</p> : null}
-			</p>
-			<p class="text">{text}</p>
+			<Navbar expand="lg">
+				<Container>
+					<Navbar.Brand href="/login">Pirate Translator</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link href="/docs">Documentation</Nav.Link>
+						{client.isAdmin ? (
+							<Nav.Link href="/admin">Admin</Nav.Link>
+						) : null}
+						<Nav.Link href="">Client ID: {client.id}</Nav.Link>
+					</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			<img class="main-logo" src="piratetext.png"></img>
+			<div class="center">
+				<img class="map" src="piratemap.png" alt="Pirate map"></img>
+				<p>
+					<input
+						value={prompt}
+						onChange={(e) => setPrompt(e.target.value)}
+						type="text"
+						placeholder="Enter The Prompt You Wish To Translate Here"
+					/>
+				</p>
+				<p>
+					<button class="text" disabled={isPending} onClick={translate}>
+						Translate
+					</button>
+					{error ? <p style={{ color: 'red' }}>{error}</p> : null}
+				</p>
+				<p class="output-text">{text}</p>
 			</div>
-			</div>
+		</div>
 		</>
 	);
 }
