6f8f0fb0d2d442949ebc52927aa747787d93d8c5
Mike <44955015+jdbool@users.noreply.github.com>
2022-04-03 22:00:13 -0700

    Clean up

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,12 @@
-import { Link, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useState } from 'react';
 import api from './api';
 
 import { useClient } from './ClientContext';
-import { Navbar, Nav, NavDropdown, Container, Offcanvas, Form, FormControl, Button} from "react-bootstrap";
-import { useSetToken } from './TokenContext';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export default function Home() {
 	const client = useClient();
-	const setToken = useSetToken();
-	const setClient = useSetClient();
 
 	const [text, setText] = useState('');
 	const [prompt, setPrompt] = useState('');
@@ -32,46 +29,46 @@ export default function Home() {
 		setIsPending(false);
 	};
 
-	 if (!client) {
-	 	return <Navigate replace to="/login" />;
-	 }
+	if (!client) {
+		return <Navigate replace to="/login" />;
+	}
 
 	return (
 		<>
-		<div class="background-body">
-		<Navbar expand="lg">
-  			<Container>
-    			<Navbar.Brand href="/login">Pirate Translator</Navbar.Brand>
-    			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-    			<Navbar.Collapse id="basic-navbar-nav">
-      			<Nav className="me-auto">
-        			<Nav.Link href="/docs">Documentation</Nav.Link>
-					{client.isAdmin ? (
-						<Nav.Link href="/admin">Admin</Nav.Link>
-					) : null}
-					<Nav.Link href="">Client ID: {client.id}</Nav.Link>
-      			</Nav>
-    			</Navbar.Collapse>
-  			</Container>
-		</Navbar>
-		<img class="main-logo" src="piratetext.png"></img>
-		<div class="center">
-		<p>
-			<input
-				value={prompt}
-				onChange={(e) => setPrompt(e.target.value)}
-				type="text"
-				placeholder="Enter The Prompt You Wish To Translate Here"
-			/>
-		</p>
-		<p>
-			<button disabled={isPending} onClick={translate}>
-				Translate
-			</button>
-			{error ? <p style={{ color: 'red' }}>{error}</p> : null}
-			</p>
-			<p class="text">{text}</p>
-			</div>
+			<div class="background-body">
+				<Navbar expand="lg">
+					<Container>
+						<Navbar.Brand href="/login">Pirate Translator</Navbar.Brand>
+						<Navbar.Toggle aria-controls="basic-navbar-nav" />
+						<Navbar.Collapse id="basic-navbar-nav">
+							<Nav className="me-auto">
+								<Nav.Link href="/docs">Documentation</Nav.Link>
+								{client.isAdmin ? (
+									<Nav.Link href="/admin">Admin</Nav.Link>
+								) : null}
+								<Nav.Link href="">Client ID: {client.id}</Nav.Link>
+							</Nav>
+						</Navbar.Collapse>
+					</Container>
+				</Navbar>
+				<img class="main-logo" src="piratetext.png"></img>
+				<div class="center">
+					<p>
+						<input
+							value={prompt}
+							onChange={(e) => setPrompt(e.target.value)}
+							type="text"
+							placeholder="Enter The Prompt You Wish To Translate Here"
+						/>
+					</p>
+					<p>
+						<button disabled={isPending} onClick={translate}>
+							Translate
+						</button>
+						{error ? <p style={{ color: 'red' }}>{error}</p> : null}
+					</p>
+					<p class="text">{text}</p>
+				</div>
 			</div>
 		</>
 	);
--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,7 @@ import api from './api';
 import { useClient, useSetClient } from './ClientContext';
 import { useSetToken } from './TokenContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, NavDropdown, Container, Offcanvas, Form, FormControl, Button} from "react-bootstrap";
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export default function Login() {
 	const setToken = useSetToken();
@@ -35,53 +35,61 @@ export default function Login() {
 	};
 
 	if (client) {
-		return <Navigate replace to="/"/>;
+		return <Navigate replace to="/" />;
 	}
 
 	return (
 		<>
-		<div class="background-body">
-			<Navbar expand="lg">
-				<Container>
-					<Navbar.Brand href="/login">Pirate Translator</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="me-auto">
-					<Nav.Link href="/docs">Documentation</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-				</Container>
-			</Navbar>
-			<img class="main-logo" src="piratetext.png" alt="Pirate text title"></img>
-			<div class="center-login">
-				<img class="background" src="piratepaper.png" alt="Pirate paper"></img>
-				<div class="content-login">
-					<h1>Login</h1>
-					<p>
-						<input
-							value={clientId}
-							onChange={(e) => setClientId(e.target.value)}
-							type="text"
-							placeholder="Client ID"
-						/>
-					</p>
-					<p>
-						<input
-							value={clientSecret}
-							onChange={(e) => setClientSecret(e.target.value)}
-							type="password"
-							placeholder="Client Secret"
-						/>
-					</p>
-					<p>
-						<button disabled={isPending} onClick={logIn}>
-							Login
-						</button>
-					</p>
-					{error ? <p style={{ color: 'red' }}>{error}</p> : null}
+			<div class="background-body">
+				<Navbar expand="lg">
+					<Container>
+						<Navbar.Brand href="/login">Pirate Translator</Navbar.Brand>
+						<Navbar.Toggle aria-controls="basic-navbar-nav" />
+						<Navbar.Collapse id="basic-navbar-nav">
+							<Nav className="me-auto">
+								<Nav.Link href="/docs">Documentation</Nav.Link>
+							</Nav>
+						</Navbar.Collapse>
+					</Container>
+				</Navbar>
+				<img
+					class="main-logo"
+					src="piratetext.png"
+					alt="Pirate text title"
+				></img>
+				<div class="center-login">
+					<img
+						class="background"
+						src="piratepaper.png"
+						alt="Pirate paper"
+					></img>
+					<div class="content-login">
+						<h1>Login</h1>
+						<p>
+							<input
+								value={clientId}
+								onChange={(e) => setClientId(e.target.value)}
+								type="text"
+								placeholder="Client ID"
+							/>
+						</p>
+						<p>
+							<input
+								value={clientSecret}
+								onChange={(e) => setClientSecret(e.target.value)}
+								type="password"
+								placeholder="Client Secret"
+							/>
+						</p>
+						<p>
+							<button disabled={isPending} onClick={logIn}>
+								Login
+							</button>
+						</p>
+						{error ? <p style={{ color: 'red' }}>{error}</p> : null}
+					</div>
 				</div>
 			</div>
-		</div>
 		</>
 	);
 }
